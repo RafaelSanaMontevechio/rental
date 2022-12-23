d2084e5b8360c0ba17efc4a2577626b7e1576751
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2022-12-23 17:43:01 -0300

    docs: :memo: Add swagger

--- src/modules/cars/useCases/categories/importCategories/ImportCategoriesUseCase.ts
+++ src/modules/cars/useCases/categories/importCategories/ImportCategoriesUseCase.ts
@@ -31,6 +31,7 @@ export class ImportCategoriesUseCase {
           });
         })
         .on('end', () => {
+          fs.promises.unlink(file.path);
           resolve(categories);
         })
         .on('error', (err) => {
